0e1c24e7da71e62283ecc96ad18c4182d50081e2
ohsnaplol <emyb@bellsouth.net>
2017-12-08 13:57:04 -0500

    added firebase config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAAZxdI1B3A8c3RPZiG8gqjk4zwCeza4wA",
+  authDomain: "rps-mp-e274f.firebaseapp.com",
+  databaseURL: "https://rps-mp-e274f.firebaseio.com",
+  projectId: "rps-mp-e274f",
+  storageBucket: "rps-mp-e274f.appspot.com",
+  messagingSenderId: "575598420973"
+};
+firebase.initializeApp(config);
+database = firebase.database();
+
 function rps(p1, p2) {
   if (p1 == p2) {
     return 'draw';
