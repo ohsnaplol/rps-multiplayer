a527003a52b83123457c62368e1c27fd290e44d9
ohsnaplol <emyb@bellsouth.net>
2017-12-11 13:12:18 -0500

    removed else statement in rps method

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,6 @@ function rps(p1, p2) {
   }
   if ((p1 == 's' && p2 == 'p') || (p1 == 'p' && p2 == 'r') || (p1 == 'r' && p2 == 's')) {
     return 'p1';
-  } else {
-    return 'p2';
   }
+  return 'p2';
 }
