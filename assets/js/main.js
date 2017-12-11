257feb3ff5710766e266c8c3d2bad9e8c5c36f51
ohsnaplol <emyb@bellsouth.net>
2017-12-11 14:05:23 -0500

    Removed semi colons

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,47 +6,47 @@ var config = {
   projectId: "rps-mp-e274f",
   storageBucket: "rps-mp-e274f.appspot.com",
   messagingSenderId: "575598420973"
-};
-firebase.initializeApp(config);
-database = firebase.database();
+}
+firebase.initializeApp(config)
+database = firebase.database()
 
-var playerID;
-var enemyID;
-var gameIsActive;
+var playerID
+var enemyID
+var gameIsActive
 
 $(document).ready(function() {
   // Constantly checking to see whether someone is already playing
   database.ref().on("value", function(snapshot) {
-    gameIsActive = snapshot.val().gameIsActive;
+    gameIsActive = snapshot.val().gameIsActive
     if (gameIsActive) {
-      $("#gameView").show();
+      $("#gameView").show()
     } else {
-      $("#gameView").hide();
+      $("#gameView").hide()
     }
-  });
+  })
   // If user has previously set a playerID in localStorage, set name field
   if (localStorage.getItem("playerID") !== null) {
-    $("#nickNameField").val(playerID);
+    $("#nickNameField").val(playerID)
   }
 
   $("#userIDSubmission").submit(function(event) {
-    event.preventDefault();
+    event.preventDefault()
     if ($("#nickNameField").val().trim() !== "" && !gameIsActive) {
-      playerID = $("#nickNameField").val().trim();
-      localStorage.setItem('playerID', playerID);
-      $("#playerTitle").text(playerID);
-      $("#userIDSubmission").hide();
-      $("#gameView").show();
+      playerID = $("#nickNameField").val().trim()
+      localStorage.setItem('playerID', playerID)
+      $("#playerTitle").text(playerID)
+      $("#userIDSubmission").hide()
+      $("#gameView").show()
     }
   })
 })
 
 function rps(p1, p2) {
   if (p1 == p2) {
-    return 'draw';
+    return 'draw'
   }
   if ((p1 == 's' && p2 == 'p') || (p1 == 'p' && p2 == 'r') || (p1 == 'r' && p2 == 's')) {
-    return 'p1';
+    return 'p1'
   }
-  return 'p2';
+  return 'p2'
 }
